27cf5e5c465442128a66da1bca360d710052f499
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2022-11-24 13:19:29 +0200

    run components in App file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-
+import {Button, Col, Container, Row} from 'react-bootstrap'
+import Input from './components/Input';
+import List from './components/List';
 
 function App() {
   return (
     <div className="App">
-
+      <Container className='p-5'>
+        <Row>
+          <Col sm='12'>
+            <div className='text-capitalize text-center fw-bold fs-3 py-4'>What's your plan today?</div>
+          </Col>
+          <Col sm='12'>
+            <Input/>
+            <List/>
+            <Button className='daleteAll fw-bold mt-3'>Delete All</Button>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
